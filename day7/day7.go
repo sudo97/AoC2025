bbfc71aa2c1db8d7de9c7470a10d500d13eee1e1
Illia Mikhnevych <i.mihnevich@gmail.com>
2025-12-07 15:27:33 +0200

    somhow solve day7

--- day7/day7.go
+++ day7/day7.go
@@ -31,13 +31,27 @@ func split(beams []int, splitters []int) ([]int, int) {
 }
 
 func Solution(reader io.Reader) {
-	beams, splitters := parseInput(reader)
+	beams, splitters, width := parseInput(reader)
 	fmt.Printf("day7, part1: %d\n", part1(beams, splitters))
-	fmt.Printf("day7, part2: %d\n", part2(reader))
+	fmt.Printf("day7, part2: %d\n", part2(beams, splitters, width))
 }
 
-func part2(io.Reader) int {
-	return 40
+func part2(beams []int, splitters [][]int, width int) int {
+	arr := make([]int, width)
+
+	for _, beam := range beams {
+		arr[beam]++
+	}
+
+	for _, sp := range splitters {
+		arr = part2Step(arr, sp)
+	}
+
+	sum := 0
+	for _, v := range arr {
+		sum += v
+	}
+	return sum
 }
 
 func part1(beams []int, splitters [][]int) int {
@@ -53,15 +67,18 @@ func part1(beams []int, splitters [][]int) int {
 	return count
 }
 
-func parseInput(r io.Reader) ([]int, [][]int) {
+func parseInput(r io.Reader) ([]int, [][]int, int) {
 	scanner := bufio.NewScanner(r)
 
 	var beams []int
+	var width int
 
 	if scanner.Scan() {
-		beams = parseInitBeam(scanner.Text())
-	} else {
-		panic("No init beam")
+		line := scanner.Text()
+
+		width = len(line)
+
+		beams = parseInitBeam(line)
 	}
 
 	splitters := [][]int{}
@@ -70,7 +87,7 @@ func parseInput(r io.Reader) ([]int, [][]int) {
 		splitters = append(splitters, parseSplitterLine(line))
 	}
 
-	return beams, splitters
+	return beams, splitters, width
 }
 
 func findAllEntries(s string, c byte) []int {
@@ -89,4 +106,21 @@ func parseInitBeam(s string) []int {
 
 func parseSplitterLine(s string) []int {
 	return findAllEntries(s, '^')
+}
+
+func part2Step(arr []int, splitters []int) []int {
+	newArr := make([]int, len(arr))
+
+	for sp, v := range arr {
+		if v != 0 {
+			if slices.Contains(splitters, sp) {
+				newArr[sp-1] += v
+
+				newArr[sp+1] += v
+			} else {
+				newArr[sp] += v
+			}
+		}
+	}
+	return newArr
 }
--- day7/day7_test.go
+++ day7/day7_test.go
@@ -114,11 +114,48 @@ func TestParseInput(t *testing.T) {
 	inp := `..S..
 ..^..`
 	exp := []int{2}
-	beams, splitters := parseInput(strings.NewReader(inp))
+	beams, splitters, width := parseInput(strings.NewReader(inp))
 	if !slices.Equal(beams, exp) {
 		t.Errorf("want %v, got %v", exp, beams)
 	}
 	if !slices.EqualFunc(splitters, [][]int{exp}, func(s1 []int, s2 []int) bool { return slices.Equal(s1, s2) }) {
 		t.Errorf("want %v, got %v", exp, splitters)
+	}
+
+	if width != 5 {
+		t.Errorf("Nah")
+	}
+}
+
+func TestPart2Step(t *testing.T) {
+	table := []struct {
+		arr  []int
+		sp   []int
+		want []int
+	}{
+		{[]int{0, 0, 1, 0, 0}, []int{2}, []int{0, 1, 0, 1, 0}},
+		{[]int{0, 1, 0, 1, 0}, []int{1, 3}, []int{1, 0, 2, 0, 1}},
+		{[]int{0, 1, 0, 2, 0}, []int{1, 3}, []int{1, 0, 3, 0, 2}},
+		{[]int{1, 1, 0, 2, 0}, []int{1, 3}, []int{2, 0, 3, 0, 2}},
+		{[]int{0, 0, 0, 0, 0, 1, 0, 2, 0, 1, 0, 0, 0, 0, 0}, []int{5, 7, 9}, []int{0, 0, 0, 0, 1, 0, 3, 0, 3, 0, 1, 0, 0, 0, 0}},
+		{[]int{0, 0, 0, 0, 1, 0, 3, 0, 3, 0, 1, 0, 0, 0, 0}, []int{4, 6, 10}, []int{0, 0, 0, 1, 0, 4, 0, 3, 3, 1, 0, 1, 0, 0, 0}},
+		{[]int{0, 0, 0, 1, 0, 4, 0, 3, 3, 1, 0, 1, 0, 0, 0}, []int{3, 5, 9, 11}, []int{0, 0, 1, 0, 5, 0, 4, 3, 4, 0, 2, 0, 1, 0, 0}},
+		{[]int{0, 0, 1, 0, 5, 0, 4, 3, 4, 0, 2, 0, 1, 0, 0}, []int{2, 6, 12}, []int{0, 1, 0, 1, 5, 4, 0, 7, 4, 0, 2, 1, 0, 1, 0}},
+	}
+
+	for _, tst := range table {
+		got := part2Step(tst.arr, tst.sp)
+
+		if !slices.Equal(got, tst.want) {
+			splitters := make([]int, len(got))
+			for i := range got {
+				if slices.Contains(tst.sp, i) {
+					splitters[i] = 1
+				} else {
+					splitters[i] = 0
+				}
+			}
+			t.Errorf("\nhad\t\t\t%v\nspl\t\t\t%v\nwant\t\t%v\ngot\t\t\t%v", tst.arr, splitters, tst.want, got)
+		}
 	}
 }
